9505cbb02613ccc628ed5572c0573d327b603beb
LE0NARD0R <laalvear@uninorte.edu.co>
2022-09-14 14:28:44 -0500

    prueba cuasi final

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
- const express = require('express');
+const express = require('express');    
+const path = require("path");
 
- const app = express();
+const app = express();
 
 //app.get('/', (req, res) => res.send('Hello World from Express!'))
 
-app.get("/TiempoReal", (req, res) => {
-    return res.sendFile(path.join(__dirname + "/paginaweb.html"));
-  });
+app.get("/paginaWeb", (req, res) => {
+   console.log(__dirname) 
+   res.sendFile(path.join(__dirname + "/paginaweb.html"));
+ });
+
+//-----------------------------------------------------------------------
 
 app.listen(8000);
 console.log('Server on port 8000')
