95133bd72fd6b5a57f17372a62897dcd74e77fd5
LE0NARD0R <laalvear@uninorte.edu.co>
2022-09-17 21:19:43 -0500

    advances of webpage

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ app.set('views', path.join(__dirname, 'views'));
 //routes
 app.use(require('./routes/routes'));
 
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//initializing server
 app.listen(8000, () => {
     console.log('Server on port 8000');
 });
